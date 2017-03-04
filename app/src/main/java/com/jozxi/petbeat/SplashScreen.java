e35cab8c984e3c1299b4c5816ca6f8f3c11ea028
Jozx <jsaracho1@gmail.com>
2017-03-04 10:08:08 -0300

    Splash Screen

---
+++ app/src/main/java/com/jozxi/petbeat/SplashScreen.java
@@ -0,0 +1,50 @@
+package com.jozxi.petbeat;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ProgressBar;
+
+/**
+ * Created by jsara on 04/03/2017.
+ */
+
+public class SplashScreen extends Activity {
+    private ProgressBar mProgress;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Show the splash screen
+        setContentView(R.layout.splash_screen);
+        mProgress = (ProgressBar) findViewById(R.id.splash_screen_progress_bar);
+
+        // Start lengthy operation in a background thread
+        new Thread(new Runnable() {
+            public void run() {
+                doWork();
+                startApp();
+                finish();
+            }
+        }).start();
+    }
+
+    private void doWork() {
+        for (int progress=0; progress<100; progress+=10) {
+            try {
+                Thread.sleep(1000);
+                mProgress.setProgress(progress);
+            } catch (Exception e) {
+                e.printStackTrace();
+                Log.i("Error ",e.toString());
+            }
+        }
+    }
+
+    private void startApp() {
+        Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+        startActivity(intent);
+    }
+}
+
