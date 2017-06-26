896eb9c505fa1da883fe8026ccd5afa12b0db7fd
Jozx <jsaracho1@gmail.com>
2017-06-26 12:09:11 -0400

    Agregado radio button para seleccionar animal

--- app/src/main/java/com/jozxi/petbeat/ConfigActivity.java
+++ app/src/main/java/com/jozxi/petbeat/ConfigActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
@@ -15,6 +16,8 @@ public class ConfigActivity extends AppCompatActivity {
 
     private static final String TAG = "ConfigActivity";
     private EditText editText;
+    private int flag;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,14 +35,27 @@ public class ConfigActivity extends AppCompatActivity {
         String idNode = editText.getText().toString();
         DatabaseReference idAndroid = FirebaseDatabase.getInstance().getReference().child(idNode).child("id_android");
         DatabaseReference id_node = FirebaseDatabase.getInstance().getReference().child(idNode).child("id");
+        DatabaseReference id_animal = FirebaseDatabase.getInstance().getReference().child(idNode).child("tipo_animal");
 
+        RadioButton radioButton = (RadioButton) findViewById(R.id.radio_perro);
+        boolean estado = radioButton.isChecked();
+
+        if (estado) {
+            flag = 1;
+        }
+        else
+        {
+            flag = 2;
+        }
 
 
         idAndroid.setValue(getIntent().getExtras().getString("token"));
         id_node.setValue(idNode);
+        id_animal.setValue(flag);
 
         Log.i(TAG, "idNode " + idNode);
         Log.d(TAG, "idAndroid " + (getIntent().getExtras().getString("token")));
+        Log.d(TAG, "id_animal " + flag);
 
         FirebaseMessaging.getInstance().subscribeToTopic(idNode);
         Log.d(TAG, "Subscribed to " + idNode);
